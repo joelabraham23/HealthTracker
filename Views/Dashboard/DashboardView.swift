4d13d87c5b250adb9ee723444d27c7ac54540d54
Joel Abraham <joelabraham2341@gmail.com>
2025-08-22 16:48:44 +1000

    Created basic tab view and navbar at the bottom

---
+++ Views/Dashboard/DashboardView.swift
@@ -0,0 +1,8 @@
+//
+//  DashboardView.swift
+//  HealthTracker
+//
+//  Created by Joel Abraham on 22/8/2025.
+//
+
+import Foundation
---
+++ Views/Dashboard/MainTabView.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabView.swift
+//  HealthTracker
+//
+//  Created by Joel Abraham on 22/8/2025.
+//
+
+import Foundation
